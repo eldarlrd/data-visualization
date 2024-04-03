0b5f384761827e83d8f827f866eb84703d227b64
eldarlrd <eldarlrd@pm.me>
2024-04-03 21:02:01 +0400

    refactor(app): restructure

--- src/App.ts
+++ src/App.ts
@@ -1,35 +1,35 @@
-import VISUALIZATIONS from '@/content/visualizations.yaml';
+import VISUALS from '@/content/visuals.yaml';
 
-interface VisualizationDetails {
+interface VisualProps {
   title: string;
   text: string;
 }
 
-export const App = (): string => {
-  const VisualizationCards = (VISUALIZATIONS as VisualizationDetails[]).map(
-    visualization => `
-  <figure style='min-height:11rem;max-height:13rem' class='justify-content-between card card-body col-sm-5 col-lg-3'>
-    <span>
-      <h4 class='card-title'>
-        ${visualization.title}
-      </h4>
-      <figcaption class='card-text'>
-        ${visualization.text}
-      </figcaption>
-    </span>
-    <a
-      style='max-width:fit-content'
-      class='mt-1 link-body-emphasis link-offset-2 link-underline-opacity-0 link-underline-opacity-75-hover user-select-none fs-5'
-      href=${visualization.title.toLowerCase().replaceAll(' ', '-')}>
-      Show
-    </a>
-  </figure>
-`
-  );
+const VisualCards = (VISUALS as VisualProps[]).map(
+  visual => `
+    <figure style='min-height:11rem;max-height:13rem' class='justify-content-between card card-body col-sm-5 col-lg-3'>
+      <span>
+        <h4 class='card-title'>
+          ${visual.title}
+        </h4>
+        <figcaption class='card-text'>
+          ${visual.text}
+        </figcaption>
+      </span>
+      <a
+        style='max-width:fit-content'
+        class='mt-1 link-body-emphasis link-offset-2 link-underline-opacity-0 link-underline-opacity-75-hover user-select-none fs-5'
+        href=${visual.title.toLowerCase().replaceAll(' ', '-')}>
+        Show
+      </a>
+    </figure>
+  `
+);
 
+export const App = (): string => {
   return `
     <main class='my-4 container row justify-content-center align-items-center column-gap-3'>
-      ${VisualizationCards.join('')}
+      ${VisualCards.join('')}
     </main>
   `;
 };
---
+++ src/features/pages/BarChart.ts
@@ -0,0 +1,5 @@
+export const BarChart = (): string => {
+  return `
+    H
+  `;
+};
